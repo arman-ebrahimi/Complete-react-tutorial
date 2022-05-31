1e8bf97d142628357cc41b6da7d96a1276e46525
arman <ebrahimi.arman72@gmail.com>
2022-05-31 18:19:16 +0430

    added modifies

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import {useEffect, useState} from "react";
+import BlogList from "./BlogList";
+
+const Home = () =>{
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+    useEffect(() => {
+        console.log("useEffect is run");
+    });
+    return(
+        <div className="Home">
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+        </div>
+    );
+}
+
+export default Home;
