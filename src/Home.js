488ae40a21a263b1c7b958a9a99aa4595f24a0fa
arman <ebrahimi.arman72@gmail.com>
2022-06-05 08:41:53 +0430

    delete .yarn && .idea

--- src/Home.js
+++ src/Home.js
@@ -7,17 +7,20 @@ const Home = () =>{
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
-
+    const [name, setName] = useState('mario');
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
     useEffect(() => {
         console.log("useEffect is run");
-    });
+        console.log(name);
+    },[name]);
     return(
         <div className="Home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <button onClick={() => setName('arman')}>Delete name</button>
+            <p>{name}</p>
         </div>
     );
 }
