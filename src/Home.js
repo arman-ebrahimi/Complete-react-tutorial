bdd1becf9b59c5f6bb30f87777eaeee33b1013cd
arman <ebrahimi.arman72@gmail.com>
2022-06-08 20:58:14 +0430

    added db and modifies to home

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,31 @@ import BlogList from "./BlogList";
 const Home = () =>{
     const [blogs, setBlogs] = useState(null);
     const [isLoaded, setIsLoaded] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => res.json())
+            fetch('/db.json')
+                .then(res => {
+                    if(!res.ok){
+                        throw Error("not receive data from server");
+                    }
+                    return res.json()
+                })
                 .then(data => {
                     setBlogs(data);
                     setIsLoaded(false);
-                });
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsLoaded(false);
+                    setError(err.message);
+                })
         }, 1000)
     }, []);
     return(
         <div className="Home">
+            {error && <div>{error}</div>}
             {isLoaded && <div>Is Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
