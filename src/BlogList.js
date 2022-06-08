75050f057a2667261a7d59148e231aa71854e729
arman <ebrahimi.arman72@gmail.com>
2022-06-08 08:10:23 +0430

    add modifies

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs, title, handleDelete}) => {
+const BlogList = ({blogs, title}) => {
     return(
         <div className="blog-list">
             <h2>{title}</h2>
@@ -6,9 +6,6 @@ const BlogList = ({blogs, title, handleDelete}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
-                    <button onClick={() => handleDelete(blog.id)}>
-                        Delete Blog
-                    </button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -2,25 +2,23 @@ import {useEffect, useState} from "react";
 import BlogList from "./BlogList";
 
 const Home = () =>{
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-    const [name, setName] = useState('mario');
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(true);
+
     useEffect(() => {
-        console.log("useEffect is run");
-        console.log(name);
-    },[name]);
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => res.json())
+                .then(data => {
+                    setBlogs(data);
+                    setIsLoaded(false);
+                });
+        }, 1000)
+    }, []);
     return(
         <div className="Home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-            <button onClick={() => setName('arman')}>Delete name</button>
-            <p>{name}</p>
+            {isLoaded && <div>Is Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
 }
